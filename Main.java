982d9041df220cdda3b98b3a6235e35a6dfbad39
edanbarcelon <edanbarcelon@gmail.com>
2026-02-02 21:33:31 +0800

    maon

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Calculator Running...");
+        
+        // Test the operations
+        System.out.println("5 + 3 = " + Addition.add(5, 3));
+        System.out.println("5 - 3 = " + Subtraction.subtract(5, 3));
+        System.out.println("5 * 3 = " + Multiplication.multiply(5, 3));
+        System.out.println("6 / 3 = " + Division.divide(6, 3));
+    }
+}
